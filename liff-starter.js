a7adcee5e0df43f80e4174eb68f76a5a2b107d15
arwutr <arwutr@hotmail.com>
2019-10-21 14:52:18 +0700

    Add files via upload

--- liff-starter.js
+++ liff-starter.js
@@ -1,45 +1,88 @@
 window.onload = function (e) {
-    liff.init(function () {
-        getP();
+    liff.init(function (data) {
+        initializeApp(data);
     });
 };
 
-// sendMessages call
-    document.getElementById('Hello').addEventListener('click', function () {
-        liff.sendMessages([{
-          type: "sticker",
-          altText: "Sticker",
-          template: {
-             type: "image_carousel",
-             columns: [{
-                 imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png",
-                 action: {
-                     type: "uri",
-                     uri: "line://shop/sticker/detail/13561"}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
+function initializeApp(data) {
+    document.getElementById('languagefield').textContent = data.language;
+    document.getElementById('viewtypefield').textContent = data.context.viewType;
+    document.getElementById('useridfield').textContent = data.context.userId;
+    document.getElementById('utouidfield').textContent = data.context.utouId;
+    document.getElementById('roomidfield').textContent = data.context.roomId;
+    document.getElementById('groupidfield').textContent = data.context.groupId;
+
+    // openWindow call
+    document.getElementById('openwindowbutton').addEventListener('click', function () {
+        liff.openWindow({
+            url: 'https://line.me'
         });
-    }
+    });
+
+    // closeWindow call
+    document.getElementById('closewindowbutton').addEventListener('click', function () {
+        liff.closeWindow();
+    });
+
+    // sendMessages call
+    document.getElementById('sendmessagebutton').addEventListener('click', function () {
+        liff.sendMessages([{
+            type: 'text',
+            text: "You've successfully sent a message! Hooray!"
+        }, {
+            type: 'sticker',
+            packageId: '1274372',
+            stickerId: '11122055'
+        }]).then(function () {
+            window.alert("Message sent");
+        }).catch(function (error) {
+            window.alert("Error sending message: " + error);
+        });
+    });
+
+    // get access token
+    document.getElementById('getaccesstoken').addEventListener('click', function () {
+        const accessToken = liff.getAccessToken();
+        document.getElementById('accesstokenfield').textContent = accessToken;
+        toggleAccessToken();
+    });
+
+    // get profile call
+    document.getElementById('getprofilebutton').addEventListener('click', function () {
+        liff.getProfile().then(function (profile) {
+            document.getElementById('useridprofilefield').textContent = profile.userId;
+            document.getElementById('displaynamefield').textContent = profile.displayName;
+
+            const profilePictureDiv = document.getElementById('profilepicturediv');
+            if (profilePictureDiv.firstElementChild) {
+                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+            }
+            const img = document.createElement('img');
+            img.src = profile.pictureUrl;
+            img.alt = "Profile Picture";
+            profilePictureDiv.appendChild(img);
+
+            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+            toggleProfileData();
+        }).catch(function (error) {
+            window.alert("Error getting profile: " + error);
+        });
+    });
 }
-                                                      
-// sendMessages call
-    document.getElementById('ดีจ้า').addEventListener('click', function () {
-        liff.sendMessages([{
-          type: "sticker",
-          altText: "Sticker",
-          template: {
-             type: "image_carousel",
-             columns: [{
-                 imageUrl: "line://app/1583925164-dpP55jjN?type=sticker&stk=pupup&sid=159360750&pkg=13561",
-                 action: {
-                     type: "uri",
-                     uri: "line://shop/sticker/detail/13561"}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
-        });
+
+function toggleAccessToken() {
+    toggleElement('accesstokendata');
+}
+
+function toggleProfileData() {
+    toggleElement('profileinfo');
+}
+
+function toggleElement(elementId) {
+    const elem = document.getElementById(elementId);
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = "none";
+    } else {
+        elem.style.display = "block";
     }
 }
