5e72e9681320e6f9a8d7c63babb01de9cf663b93
arwutr <arwutr@hotmail.com>
2019-10-21 16:38:06 +0700

    Add files via upload

--- liff-starter.js
+++ liff-starter.js
@@ -14,38 +14,14 @@ function initializeApp(data) {
 
     // openWindow call
     document.getElementById('openwindowbutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'template',
-          altText: 'Sticker',
-          template: {
-             type: 'image_carousel',
-             columns: [{
-                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png',
-                 action: {
-                     type: 'uri',
-                     uri: 'line://shop/sticker/detail/13561'}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
+        liff.openWindow({
+            url: 'https://line.me'
+        });
     });
 
     // closeWindow call
     document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'template',
-          altText: 'Sticker',
-          template: {
-             type: 'image_carousel',
-             columns: [{
-                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png',
-                 action: {
-                     type: 'uri',
-                     uri: 'line://shop/sticker/detail/13561'}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
+        liff.closeWindow();
     });
 
     // sendMessages call
@@ -64,43 +40,52 @@ function initializeApp(data) {
                         }
         }]).then(function () {
             liff.closeWindow();
+        });
     });
 
     // get access token
     document.getElementById('getaccesstoken').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'template',
-          altText: 'Sticker',
-          template: {
-             type: 'image_carousel',
-             columns: [{
-                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png',
-                 action: {
-                     type: 'uri',
-                     uri: 'line://shop/sticker/detail/13561'}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
+        const accessToken = liff.getAccessToken();
+        document.getElementById('accesstokenfield').textContent = accessToken;
+        toggleAccessToken();
     });
 
     // get profile call
     document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'template',
-          altText: 'Sticker',
-          template: {
-             type: 'image_carousel',
-             columns: [{
-                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png',
-                 action: {
-                     type: 'uri',
-                     uri: 'line://shop/sticker/detail/13561'}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
+        liff.getProfile().then(function (profile) {
+            document.getElementById('useridprofilefield').textContent = profile.userId;
+            document.getElementById('displaynamefield').textContent = profile.displayName;
+
+            const profilePictureDiv = document.getElementById('profilepicturediv');
+            if (profilePictureDiv.firstElementChild) {
+                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+            }
+            const img = document.createElement('img');
+            img.src = profile.pictureUrl;
+            img.alt = "Profile Picture";
+            profilePictureDiv.appendChild(img);
+
+            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+            toggleProfileData();
+        }).catch(function (error) {
+            window.alert("Error getting profile: " + error);
         });
-      });
+    });
+}
+
+function toggleAccessToken() {
+    toggleElement('accesstokendata');
+}
+
+function toggleProfileData() {
+    toggleElement('profileinfo');
+}
+
+function toggleElement(elementId) {
+    const elem = document.getElementById(elementId);
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = "none";
+    } else {
+        elem.style.display = "block";
     }
 }
