0c3afcf774a7ef73826f19e4f6c37ee402f55aef
arwutr <arwutr@hotmail.com>
2019-10-21 16:51:31 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -43,6 +43,25 @@ function initializeApp(data) {
         });
     });
 
+      // sendMessages call2
+    document.getElementById('sendmessagebutton2').addEventListener('click', function () {
+        liff.sendMessages([{
+          type: 'template',
+          altText: 'Sticker',
+          template: {
+             type: 'image_carousel',
+             columns: [{
+                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360751/IOS/sticker_popup.png',
+                 action: {
+                     type: 'uri',
+                     uri: 'line://shop/sticker/detail/13561'}}
+                          ]
+                        }
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    });  
+    
     // get access token
     document.getElementById('getaccesstoken').addEventListener('click', function () {
         const accessToken = liff.getAccessToken();
