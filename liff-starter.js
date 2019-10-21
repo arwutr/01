7a2f0b78b370349440684337d1b2130d301ca114
arwutr <arwutr@hotmail.com>
2019-10-21 14:37:33 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -7,7 +7,11 @@ window.onload = function (e) {
 // sendMessages call
     document.getElementById('HELLO').addEventListener('click', function () {
         liff.sendMessages([{
-            type: "HELLO",
+          type: "template",
+          altText: "Sticker",
+          template: {
+             type: "image_carousel",
+             columns: [{
                  imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png",
                  action: {
                      type: "uri",
